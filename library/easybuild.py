8fb868ef05b35d44a38b84280635bfd7ae187768
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2016-10-07 16:58:20 +0200

    Add working command runner

--- library/easybuild.py
+++ library/easybuild.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python
 
-import datetime
 import json
-import sys
-import shlex
 
-date = str(datetime.datetime.now())
+from ansible.module_utils.basic import *
 
-args_file = sys.argv[1]
-args_data = file(args_file).read()
+module = AnsibleModule(
+    argument_spec={
+        'name': {'required': False, 'type': 'str'},
+        'version': {'default': None, 'required': False, 'type': 'str'},
+        'state': {
+            'default': 'present',
+            'required': False,
+            'choices': ['present', 'absent', 'latest']
+        },
+        'channels': {'default': None, 'required': False},
+        'executable': {'default': None, 'required': False},
+        'extra_args': {'default': None, 'required': False, 'type': 'str'}
+    },
+    supports_check_mode=True)
 
-arguments = {}
-deps = []
-for arg in shlex.split(args_data):
+eb_command = "bash -c \"module load EasyBuild && eb --help\""
 
-    if "=" in arg:
-        (key, value) = arg.split("=")
-        arguments[key] = value
-        if key == "deps":
-            deps = (value.split(","))
+(rc, stdout, stderr) =  module.run_command(eb_command)
 
 print json.dumps({
-    "arguments" : arguments,
-    "deps": deps,
-    "changed" : True
+    "arguments" : module.params,
+    "changed" : True,
+    "exitcode": rc
+    # "stdout": stdout,
+    # "stderr": stderr
 })
